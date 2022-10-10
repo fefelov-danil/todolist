fe6551f75afe7cfb293826ee17cdb7fa6e3c8ee0
Daniil Fefelov <fefelov.danil@gmail.com>
2022-10-10 16:32:28 +0600

    refactor: store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import {AnyAction, combineReducers} from "redux";
 import {todoListsReducer} from "components/todolists/reducers/todolist-reducer";
-import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
+import thunk, {ThunkAction} from "redux-thunk";
 import {appReducer} from "app/app-reducer";
 import {authReducer} from "components/features/login/authReducer";
 import {configureStore} from "@reduxjs/toolkit";
@@ -13,7 +13,7 @@ const rootReducer = combineReducers({
     login: authReducer
 })
 
-export type RootReducerType  = ReturnType<typeof rootReducer>
+export type RootReducerType = ReturnType<typeof rootReducer>
 
 export const store = configureStore({
     reducer: rootReducer,
@@ -21,10 +21,8 @@ export const store = configureStore({
 })
 
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
+export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
-
-// export type AllActionsType = AppActionsType | TodoListsActionsType | TasksActionsType | LoginActionsType
 
 // @ts-ignore
 window.store = store
