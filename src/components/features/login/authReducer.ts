3bed55e78281a23330f3db65f894f742b9942107
Daniil Fefelov <fefelov.danil@gmail.com>
2022-10-13 11:27:26 +0600

    refactor: extraReducers

--- src/components/features/login/authReducer.ts
+++ src/components/features/login/authReducer.ts
@@ -3,36 +3,50 @@ import {authAPI, AuthValues} from "api/authAPI";
 import {setAppAuthLoadingAC, setAppErrorAC, setAppStatusAC} from "app/app-reducer";
 import {handleServerNetworkAppError} from "utils/error-utils";
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {AxiosError} from "axios";
 
-export const loginTC = createAsyncThunk('auth/login', async (values: AuthValues, thunkAPI) => {
-    thunkAPI.dispatch(setAppStatusAC('loading'))
-    const res = await authAPI.login(values)
+
+export const verifyLoginTC = createAsyncThunk('auth/verifyLogin', async (payload, thunkAPI) => {
+    thunkAPI.dispatch(setAppAuthLoadingAC(true))
+    const res = await authAPI.verifyLogin()
+    thunkAPI.dispatch(setAppAuthLoadingAC(false))
     if (res.data.resultCode === 0) {
-        thunkAPI.dispatch(loginAC(true))
-        thunkAPI.dispatch(verifyLoginAC(true))
+        return true
     } else {
-        thunkAPI.dispatch(setAppErrorAC({appError: res.data.messages[0]}))
+        return false
     }
 })
-
-export const loginTC1 = (values: AuthValues) => (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC('loading'))
-    authAPI.login(values)
-        .then((res) => {
-            if (res.data.resultCode === 0) {
-                dispatch(loginAC(true))
-                dispatch(verifyLoginAC(true))
-            } else {
-                dispatch(setAppErrorAC({appError: res.data.messages[0]}))
-            }
-        })
-        .catch(error => {
-            handleServerNetworkAppError(dispatch, error)
-        })
-        .finally(() => {
-            dispatch(setAppStatusAC('idle'))
-        })
-}
+export const loginTC = createAsyncThunk('auth/login', async (values: AuthValues, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC('loading'))
+    try {
+        const res = await authAPI.login(values)
+        if (res.data.resultCode === 0) {
+            thunkAPI.dispatch(verifyLoginTC.fulfilled(true, ''))
+            thunkAPI.dispatch(setAppStatusAC('succeeded'))
+            return true
+        } else {
+            thunkAPI.dispatch(setAppErrorAC({appError: res.data.messages[0]}))
+        }
+    } catch (e) {
+        const error = e as Error | AxiosError<{ error: string }>
+        handleServerNetworkAppError(thunkAPI.dispatch, error)
+    }
+    thunkAPI.dispatch(setAppStatusAC('idle'))
+})
+export const logoutTC = createAsyncThunk('auth/logout', async (payload, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC('loading'))
+    try {
+        const res = await authAPI.logout()
+        if(res.data.resultCode === 0) {
+            thunkAPI.dispatch(setAppStatusAC('succeeded'))
+            return
+        }
+    } catch (e) {
+        const error = e as Error | AxiosError<{ error: string }>
+        handleServerNetworkAppError(thunkAPI.dispatch, error)
+    }
+    thunkAPI.dispatch(setAppStatusAC('idle'))
+})
 
 const slice = createSlice({
     name: 'auth',
@@ -40,46 +54,20 @@ const slice = createSlice({
         isLoggedIn: false,
         isVerifyLogin: false
     },
-    reducers: {
-        loginAC(state, action: PayloadAction<boolean>) {
-            state.isLoggedIn = action.payload
-        },
-        verifyLoginAC(state, action: PayloadAction<boolean>) {
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(loginTC.fulfilled, (state, action) => {
+            if (action.payload) {
+                state.isLoggedIn = action.payload
+            }
+        })
+        builder.addCase(logoutTC.fulfilled, (state, action) => {
+            state.isVerifyLogin = false
+        })
+        builder.addCase(verifyLoginTC.fulfilled, (state, action) => {
             state.isVerifyLogin = action.payload
-        }
+        })
     }
 })
 
-export const {loginAC, verifyLoginAC} = slice.actions
 export const authReducer = slice.reducer
-
-// Thunks
-export const verifyLoginTC = () => (dispatch: Dispatch) => {
-    dispatch(setAppAuthLoadingAC(true))
-    authAPI.verifyLogin()
-        .then((res) => {
-            if (res.data.resultCode === 0) {
-                dispatch(verifyLoginAC(true))
-            } else {
-                dispatch(verifyLoginAC(false))
-            }
-        })
-        .finally(() => {
-            dispatch(setAppAuthLoadingAC(false))
-        })
-}
-export const logoutTC = () => (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC('loading'))
-    authAPI.logout()
-        .then((res) => {
-            if(res.data.resultCode === 0) {
-                dispatch(verifyLoginAC(false))
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkAppError(dispatch, error)
-        })
-        .finally(() => {
-            dispatch(setAppStatusAC('idle'))
-        })
-}
